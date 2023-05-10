d4c686846cf21abf7c6f8028f0b2772c4d738b85
ramzeth25 <ramzeth25@gmail.com>
2023-05-10 08:22:22 +0000

    Remove old staff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&oe8lz2y@2f7%!eqe%xcdm+&*ub21^(%#=&(oa+4bmr*awa61p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ramzeth25.pythonanywhere.com']
 
 
 # Application definition
@@ -112,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "media/"
 
